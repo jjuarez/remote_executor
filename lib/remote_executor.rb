f62e3ff0867bf8770a7f57ff8e4e224ff54589c3
Javier Juarez <javier.juarez@gmail.com>
2010-11-20 09:04:01 +0100

    Changes in the exception management an library path

--- lib/remote_executor.rb
+++ lib/remote_executor.rb
@@ -1,6 +1,13 @@
 begin
   %w[rubygems yaml singleton choice net/ssh].each { |gem| require gem }
-  %w[version options_parser config cli].each { |lib| require "remote_executor/#{lib}"}
-rescue LoadException => e
-  $stderr.puts e.message
+rescue LoadError => e
+  fail "Some gem dependencies can´t be loaded: #{e.message}" 
+end
+
+$:.unshift File.join( File.dirname( __FILE__), 'remote_executor' )
+
+begin
+  %w[version config options_parser cli].each { |lib| require lib }
+rescue LoadError => e
+  fail "Some lib dependencies can´t be loaded: #{e.message}" 
 end
